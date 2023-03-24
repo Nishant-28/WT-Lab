2f424295a37ec477440d544c4f3c56b294dc50ad
Nishant-28 <92144362+Nishant-28@users.noreply.github.com>
2023-03-24 09:35:26 +0530

    S

--- LAB 8/q2.java
+++ LAB 8/q2.java
@@ -1,5 +1,3 @@
-// Define an interface Motor with a data member – capacity and two methods such as run() and consume(). Define a Java class “Washing machine” which implements this interface and write the code to check the value of the interface data member thru an object of the class
-
 import java.util.Scanner;
 
 interface Motor
